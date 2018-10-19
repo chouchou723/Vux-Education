bbceb3e9f55bdb594f3040efcbcce9ed571c54b9
chouchou <zhangchao@artree.net.cn>
2018-10-19 17:03:52 +0800

    test

--- src/views/courseInfo.vue
+++ src/views/courseInfo.vue
@@ -50,7 +50,7 @@
 						<div class="pho"><img :src="`${apiUrl}/attach/img/${detail.teacherPid}`" alt=""></div>
 						<div class="info">
 							<div class="name">{{detail.teacher}}</div>
-							<div class="schoolAge">{{detail.experience.label}}年教齡</div>
+							<div class="schoolAge">{{detail.experience.label}}教齡</div>
 							<p>{{detail.edu}}</p>
 						</div>
 					</div>
