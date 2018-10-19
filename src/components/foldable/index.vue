eac472744f8fa20de1095fc9361363ef54e8c049
chouchou <zhangchao@artree.net.cn>
2018-10-19 13:40:00 +0800

    test

--- src/components/foldable/index.vue
+++ src/components/foldable/index.vue
@@ -142,6 +142,7 @@
             count++
             newHeight = el.scrollHeight
             if (oldHeight !== newHeight) {
+                console.log(oldHeight,newHeight)
                 callback(newHeight)
                 if (poller) {
                     clearTimeout(poller)
