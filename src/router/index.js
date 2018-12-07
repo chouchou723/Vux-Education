29fde73909b87e5160c82ea52de6e0284714641e
chouchou <zhangchao@artree.net.cn>
2018-12-07 14:12:29 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -60,7 +60,7 @@ router.beforeEach(function (to, from, next) {
         }).then(() => {
             getStudentInfoF(next, getStudentInfo, store)
         })
-      }else if(to.meta.type == 'teacher' && !tInfo){
+      }else if(to.meta.type == 'teacher' && !tInfo||to.meta.type == 'assistant' ){
         getTokenInfo(to.meta.type, {
           code: to.query.code,
           state:to.query.state
