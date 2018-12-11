1cbb6fabaa81ae0ad46137681c3c1a3465f53592
chouchou <zhangchao@artree.net.cn>
2018-12-11 17:05:17 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -70,6 +70,30 @@
       //   return result;
       // }
     },
+    mounted(){
+      window.alert = function (name) {
+      var iframe = document.createElement("IFRAME");
+      iframe.style.display = "none";
+      iframe.setAttribute("src", 'data:text/plain,');
+      document.documentElement.appendChild(iframe);
+      window.frames[0].window.alert(name);
+      iframe.parentNode.removeChild(iframe);
+    }
+    var count = sessionStorage.getItem('count') || 0;
+    var setReload = setInterval(function () {
+      var isA = document.getElementById('isApp');
+      if(isA){
+        var el = document.getElementById('isApp').children.length;
+        if (!el && count === 0) {
+          window.alert('页面重新加载')
+          window.location.reload();
+          sessionStorage.setItem('count', 1)
+        } else {
+          clearInterval(setReload)
+        }
+      }
+    }, 2000)
+    },
     methods: {
       // touchstart($event) {
       //   this.clientX = $event.touches[0].clientX;
