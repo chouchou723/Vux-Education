d271314837f4c1c2eb5c7167620cd4c0f8ae671b
chouchou <zhangchao@artree.net.cn>
2018-12-26 16:24:43 +0800

    test

--- src/views/bindCell.vue
+++ src/views/bindCell.vue
@@ -14,7 +14,7 @@
       <x-dialog v-model="showHideOnBlur" class="dialog-demo" hide-on-blur>
         <div class="img-box">
           <div class="motoTitle">请输入随机验证码</div>
-          <x-input title="" placeholder="请输入(不区分大小写)" v-model="valueM" class="randomN" :max='4' >
+          <x-input title="" placeholder="请输入(不区分大小写)" v-model="valueM" class="randomN" :max='4' ref="xxx" @on-blur="onBlur">
             <img slot="right-full-height" :src="picCap" @click="changeAlpha">
             <!-- <div slot="right-full-height" class="randomMoto" @click="changeAlpha">
                   <span v-for="(r,index) in randomE" :key="'rr'+index">{{r}}</span>
@@ -165,6 +165,10 @@
           if (this.value.length === 11) {
             this.changeAlpha();
             this.showHideOnBlur = true;
+            console.log(this.$refs.xxx)
+            setTimeout(()=>{
+            console.log(this.$refs.xxx)
+            },100)
           } else if (this.value && this.value.length < 11) {
             this.$vux.toast.show({
               text: '请填写正确的手机号',
